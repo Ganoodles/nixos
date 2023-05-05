450d65f2b7efd1f1e7473f93ff440a0a84159358
Ganoodles <swajabbadaja@gmail.com>
2023-05-04 21:03:48 -0700

    Add virtualbox

--- system/configuration.nix
+++ system/configuration.nix
@@ -5,6 +5,7 @@
 { config, pkgs, ... }:
 
 {
+  # this should be replaced depending on the system
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -100,10 +101,11 @@
   xdg.portal.wlr.enable = true;
   # xdg.portal.enable = true;
 
-  # Enables the KDE Plasma Desktop Environment.
+  # Enables the Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
   # services.gnome.core-utilities.enable = false;
+
   # services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
@@ -117,7 +119,6 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -125,13 +126,18 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
+  
+  # enable virtualbox
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
